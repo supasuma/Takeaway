302b5ac0eac2a05e042dcc5186f014d92a9904d2
supasuma <supasuma@gmail.com>
2016-11-17 13:05:56 +0000

    added Order object user is now able to select items from menu and add to order. Tests passing.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,8 +1,10 @@
+require 'order'
 class Takeway
 
 # menu is a required object in this dependency injection so no other option provided
-  def initialize(menu:)
+  def initialize(menu:, order: nil)
     @menu = menu
+    @order = order || Order.new
   end
 
 
@@ -10,7 +12,13 @@ class Takeway
     menu.print
   end
 
+  def place_order(dishes)
+    dishes.each do |dish, quantity|
+      order.add(dish, quantity)
+    end
+  end
+
   private
 
-  attr_reader :menu
+  attr_reader :menu, :order
 end
--- spec/takeway_spec.rb
+++ spec/takeway_spec.rb
@@ -1,11 +1,19 @@
 require "takeaway"
 
 describe Takeway do
-  subject(:takeaway) { described_class.new(menu: menu) }
+  subject(:takeaway) { described_class.new(menu: menu, order: order) }
   let(:menu) { double(:menu, print: printed_menu) }
+  let(:order) { double(:order) }
   let(:printed_menu) { "Chicken: £3.50" }
+
+  let(:dishes) { {chicken: 2, fish: 1} }
 
   it "shows the menu with the dishes and prices" do
     expect(takeaway.print_menu).to eq(printed_menu)
+  end
+
+  it "can order some number of available dishes" do
+    expect(order).to receive(:add).twice
+    expect(takeaway.place_order(dishes))
   end
 end
